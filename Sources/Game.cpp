33061b298f37b9b1b41af04b38c8974bae9de78a
hw16a185 <hw16a185@oecu.jp>
2017-11-21 16:24:02 +0900

    cとd

--- Sources/Game.cpp
+++ Sources/Game.cpp
@@ -3,8 +3,8 @@
 
 // TODO: 砲台の位置を画面左に、ターゲットの位置を画面右に移動させる。(A)
 // TODO: 雲の位置を左から右に動かす。見えなくなったら左端に戻す。(B)
-// TODO: 砲台を青い壁に沿って上下に動かす。(C)
-// TODO: 弾のスピードを速くし、弾が画面右端を通り越したら再度発射可能にする。(D)
+// TODO: 砲台を青い壁に沿って上下に動かす。(C)hw16a185松本聖司
+// TODO: 弾のスピードを速くし、弾が画面右端を通り越したら再度発射可能にする。(D)hw16a185松本聖司
 // TODO: スコアのサイズを大きくする。(E)
 // TODO: スコアを100点ずつ加算するようにし、5桁の表示に変える。(F)
 // TODO: PlayBGM()関数を使って、BGMを再生する。(G)
@@ -16,7 +16,7 @@ Vector2 cannonPos;      //!< 砲台の位置
 Vector2 bulletPos;      //!< 弾の位置
 Rect    targetRect;     //!< ターゲットの矩形
 int     score;          //!< スコア
-
+int     cannonPos_y_speed;//!< 砲台の移動速度
 
 // ゲーム開始時に呼ばれる関数です。
 void Start()
@@ -26,6 +26,7 @@ void Start()
     targetRect = Rect(80, -140, 40, 40);
     bulletPos.x = -999;
     score = 0;
+    cannonPos_y_speed=3;
 }
 
 // 1/60秒ごとに呼ばれる関数です。モデルの更新と画面の描画を行います。
@@ -35,38 +36,45 @@ void Update()
     if (bulletPos.x <= -999 && Input::GetKeyDown(KeyMask::Space)) {
         bulletPos = cannonPos + Vector2(50, 10);
     }
-
+    // 砲台の上下移動
+    if(cannonPos.y>-70){
+        cannonPos_y_speed=-3;
+    }
+    if(cannonPos.y<-150){
+        cannonPos_y_speed=3;
+    }
+    cannonPos += Vector2(0, cannonPos_y_speed);
     // 弾の移動
     if (bulletPos.x > -999) {
-        bulletPos.x += 10 * Time::deltaTime;
-
+        bulletPos.x += 5000 * Time::deltaTime;
         // ターゲットと弾の当たり判定
         Rect bulletRect(bulletPos, Vector2(32, 20));
         if (targetRect.Overlaps(bulletRect)) {
             score += 1;         // スコアの加算
             bulletPos.x = -999; // 弾を発射可能な状態に戻す
         }
+        if (bulletPos.x > 300)bulletPos.x = -999;
     }
-
+    
     // 背景の描画
     Clear(Color::cyan);
     FillRect(Rect(-320, -240, 640, 100), Color::green);
-
+    
     // 雲の描画
     DrawImage("cloud1.png", cloudPos);
-
+    
     // 弾の描画
     if (bulletPos.x > -999) {
         DrawImage("bullet.png", bulletPos);
     }
-
+    
     // 砲台の描画
     FillRect(Rect(cannonPos.x-10, -140, 20, 100), Color::blue);
     DrawImage("cannon.png", cannonPos);
-
+    
     // ターゲットの描画
     FillRect(targetRect, Color::red);
-
+    
     // スコアの描画
     SetFont("nicoca_v1.ttf", 20.0f);
     DrawText(FormatString("%02d", score), Vector2(-319, 199), Color::black);
